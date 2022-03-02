ead1e033cb1af89a9413093b223262d566fffc43
ashongwurei <ashongwurei@gmail.com>
2022-03-02 14:37:42 +0100

    my-todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,61 +1,72 @@
+<!-- PROJECT STRUCTURE -->
 <template>
-  <div class="container" style="max-width: 550px">
-    <!-- HEADING -->
-    <h2 class="text-center display-2 mt-5 mb-3">Todo List</h2>
+  <div class="container d-flex justify-content-center align-items-center">
+    <div class="content">
+      <!-- Heading -->
+      <h1 class="text-center display-1 mb-5">My Todo</h1>
 
-    <!-- INPUT -->
-    <div class="d-flex">
-      <input
-        v-model="task"
-        type="text"
-        placeholder="Enter a task"
-        class="form-control rounded-0"
-      />
-      <button @click="addTask" class="btn btn-warning rounded-0">ADD</button>
-    </div>
+      <!-- Input -->
+      <div class="d-flex">
+        <input
+          v-model="task"
+          type="text"
+          placeholder="Enter a task"
+          class="form-control rounded-0"
+        />
+        <button @click="addTask" class="btn btn-warning rounded-0">ADD</button>
+      </div>
 
-    <!-- TASK TABLE -->
-    <table class="table table-bordered mt-5">
-      <thead>
-        <tr>
-          <th scope="col">Task</th>
-          <th scope="col">Status</th>
-          <th scope="col" class="text-center">Edit</th>
-          <th scope="col" class="text-center">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(task, index) in tasks" :key="index">
-          <td>{{ task.name }}</td>
-          <td style="width: 120px">
-            <span
-              @click="changeStatus(index)"
-              class="pointer"
-              :class="{
-                'text-danger': task.status === 'Pending',
-                'text-warning': task.status === 'In-progress',
-                'text-success': task.status === 'Completed',
-              }"
+      <!-- Task Table -->
+      <table class="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <th scope="col">Task</th>
+            <th scope="col">Status</th>
+            <th scope="col" class="text-center">
+              Edit
+            </th>
+            <th scope="col" class="text-center">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(task, index) in offlineTasks" :key="index">
+            <td>{{ task.name }}</td>
+            <td
+              style="width: 120px"
+              data-toggle="tooltip"
+              data-placement="right"
+              title="Click to change the status of the task"
             >
-              {{ task.status }}
-            </span>
-          </td>
-          <td>
-            <div class="text-center" @click="editTask(index)">
-              <span class="fa fa-pen pointer"></span>
-            </div>
-          </td>
-          <td>
-            <div class="text-center" @click="deleteTask(index)">
-              <span class="fa fa-trash pointer"></span>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              <span
+                @click="changeStatus(index)"
+                class="pointer"
+                :class="{
+                  'text-danger': task.status === 'Pending',
+                  'text-warning': task.status === 'In-progress',
+                  'text-success': task.status === 'Completed',
+                }"
+              >
+                {{ task.status }}
+              </span>
+            </td>
+            <td>
+              <div class="text-center" @click="editTask(index)">
+                <span class="fa fa-pen pointer"></span>
+              </div>
+            </td>
+            <td>
+              <div class="text-center" @click="deleteTask(index)">
+                <span class="fa fa-trash pointer"></span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
+<!-- PROJECT LOGIC -->
 <script>
 export default {
   name: "TodoList",
@@ -66,36 +77,21 @@ export default {
   data() {
     return {
       task: "",
+      offlineTasks: "",
       editedTask: null,
       availableStatuses: ["Pending", "In-progress", "Completed"],
-
-      tasks: [
-        {
-          name: `Do Nanle's Assignment`,
-          status: "Completed",
-        },
-        {
-          name: "Proofread my Thesis Report",
-          status: "In-progress",
-        },
-        {
-          name: "Design the UI for Modish Concept",
-          status: "Pending",
-        },
-      ],
+      tasks: [],
     };
   },
 
   methods: {
-    /* ADD/UPDATE TASK */
+    /* Add/Update Task */
     addTask() {
-      /* Warinig for Empty Input Field */
+      /* Empty Input Field */
       if (!this.task) {
         alert("Enter a Task");
         return;
       }
-
-      if (this.tasks.length === 0) return;
 
       /* Update Task */
       if (this.editedTask === null) {
@@ -103,38 +99,73 @@ export default {
           name: this.task,
           status: "Pending",
         });
+        this.saveTasks();
       } else {
         /* Add New Task */
-        this.tasks[this.editedTask].name = this.task;
+        this.offlineTasks[this.editedTask].name = this.task;
         this.editedTask = null;
+        this.saveTasks();
       }
 
       this.task = "";
     },
 
-    /* DELETE TASK */
+    /* Delete Task */
     deleteTask(index) {
-      this.tasks.splice(index, 1);
+      this.offlineTasks.splice(index, 1);
+      this.saveTasks();
     },
 
-    /* EDIT TASK */
+    /* Edit Task */
     editTask(index) {
-      this.task = this.tasks[index].name;
+      this.task = this.offlineTasks[index].name;
       this.editedTask = index;
     },
 
-    /* CHANGE TASK STATUS */
+    /* Change Task Status */
     changeStatus(index) {
-      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      let newIndex = this.availableStatuses.indexOf(
+        this.offlineTasks[index].status
+      );
+
       if (++newIndex > 2) newIndex = 0;
-      this.tasks[index].status = this.availableStatuses[newIndex];
+      this.offlineTasks[index].status = this.availableStatuses[newIndex];
+      this.saveTasks();
     },
+
+    saveTasks() {
+      let newArray = this.tasks.concat(this.offlineTasks);
+      localStorage.setItem("tasks", JSON.stringify(newArray));
+      this.getTasks();
+      this.tasks = [];
+    },
+
+    getTasks() {
+      this.offlineTasks = JSON.parse(localStorage.getItem("tasks"));
+    },
+  },
+
+  created() {
+    this.getTasks();
   },
 };
 </script>
 
-<style scoped>
+<style>
+.container {
+  min-width: 100vw;
+  min-height: 100vh;
+}
+
 .pointer {
   cursor: pointer;
+}
+
+.content {
+  background: #c3c9ff;
+  width: 50vw;
+  padding: 40px;
+  border-radius: 5px;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
